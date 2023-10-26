45ca7df86f9a8c1cfc79efcf0da847f06e8093fc
Geniusjun6 <chzhgod@gmail.com>
2023-10-26 12:12:15 +0900

    1차 작업중

--- src/search.js
+++ src/search.js
@@ -1,5 +1,4 @@
 // card.js에서 가져오는 함수
-import { fetchMovieData } from "./card.js"; // 카드 뿌려주는 함수
 
 // DOM
 const $searchBox = document.querySelector(".search_box");
@@ -11,24 +10,39 @@ const $topTitle = document.querySelector('.search_title');
 
 
 // 검색 시 새로고침 방지
-$searchBox.addEventListener("submit",function(event){
-    event.preventDefault();
-    searchedMovie();
+$searchBox.addEventListener("submit", function (event) {
+  event.preventDefault();
+  searchedMovie();
 });
 
 // 검색된 카드 리스트를 뿌려주는 함수.
 let searchedMovie = () => {
-    if ($searchInput.value.length === 0) {
-        alert("검색어가 입력되지 않았습니다.")
-        return
-    } else {
-        alert(($searchInput.value) + "를(을) 검색한 결과 입니다.");
-        $rank_mainBoard.style.display = "none"
-        $rankWrap.style.display = "none"
+  if ($searchInput.value.length === 0) {
+    alert("검색어가 입력되지 않았습니다.")
+    return
+  } else {
+    alert(($searchInput.value) + "를(을) 검색한 결과 입니다.");
+    $rank_mainBoard.style.display = "none"
+    $rankWrap.style.display = "none"
 
-        // return generateMovieCards(searchedList);
-    };
+    // return generateMovieCards(searchedList);
+  };
 };
+
+// 카드 정보들 다시 가져오기
+async function fetchMovieData() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiY2U4ODA3ZDNkNGUyZTVhY2IxYzk5Mzc2OWQ5N2E3OSIsInN1YiI6IjY1MmYzNjZhYTgwMjM2MDEzNzY4OWE2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GNyZMpov_2bKO1zPaS3oSUpaq9_6gzs1ULOjbuB1vds"
+    }
+  };
+}
+
+
+
 
 
 // 검색어와 타이틀을 비교하여 저장
@@ -36,36 +50,22 @@ let searchedList = [];
 const cardArray = Array.from($cardList);
 console.log(cardArray);
 
-$searchInput.addEventListener('submit',(event) => {
-    const value = $searchInput.value.toLowerCase();
-    searchedList = cardArray.filter(card => {
-        console.log(card);
-        const movieTitleElement = card.querySelector('.card_title');
-        if (movieTitleElement) {
-        const movieTitle = movieTitleElement.textContent.toLocaleLowerCase();
-        return movieTitle.includes(value) ? card : "";
-        }  
-    })
+$searchInput.addEventListener('submit', (event) => {
+  const value = $searchInput.value.toLowerCase();
+  searchedList = cardArray.filter(card => {
+    console.log(card);
+    const movieTitleElement = card.querySelector('.card_title');
+    if (movieTitleElement) {
+      const movieTitle = movieTitleElement.textContent.toLocaleLowerCase();
+      return movieTitle.includes(value) ? card : "";
+    }
+  })
 })
 
-// let searchKeyword = $searchInput.value;
 
-// const movieSearch = (searchKeyword) => {
-    
-//     const $movieAllList = document.querySelectorAll(".card_li");
-
-//     $movieAllList.forEach(card => {
-//         const title = card.querySelector(".card_title").textContent();
-//         if (title.includes(searchKeyword)){
-//             card.style.display = "block";
-//         } else{
-//             card.style.display = "none";
-//         };
-//     });
-// };
 
 
 // 상단 띠배너 타이틀 클릭 시 초기화면(새로고침)
 $topTitle.addEventListener('click', function () {
-    location.reload();
+  location.reload();
 });
