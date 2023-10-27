96db6e4debf83049a7116dc3644093f6e7ae22bd
Junsik <chzhgod@gmail.com>
2023-10-27 15:27:16 +0900

    아영님 포함

--- src/review.js
+++ src/review.js
@@ -26,35 +26,9 @@ $star_grade_radio.forEach(function (radio) {
   });
 });
 
-// $radio_buttons.forEach(radioButton => {
-//   radioButton.addEventListener('change', function () {
-//     if (this.checked) {
-//       const imageSource = this.nextElementSibling.getAttribute('data-image');
-//       $id_radio_image.src = imageSource;
-//     }
-//   });
-// });
-
-// $star_grade_radio.forEach(function (radio) {
-//   radio.addEventListener("hover", function () {
-//     const starScore = radio.value;
-//     $id_select_star_grade.innerText = starScore;
-//   });
-// });
-
-
-
-
 // 현재 시간
 const currentTime = () => {
   const currentDate = new Date();
-  // const year = currentDate.getFullYear();
-  // const month = currentDate.getMonth() + 1; // 월은 0부터 시작하므로 +1
-  // const day = currentDate.getDate();
-  // const hours = currentDate.getHours();
-  // const minutes = currentDate.getMinutes();
-  // const seconds = currentDate.getSeconds();
-  // return `(${year}.${month}.${day}   ${hours}:${minutes}:${seconds})` // 데이터 활용할 일 없으므로 텍스트로 저장해버림!!!
   return currentDate;
 }
 
@@ -100,6 +74,9 @@ $id_submit_btn.addEventListener("click", function (event) {
   }
 });
 
+let commentsMap;
+let valuesIterator;
+let sortedCommentsMap;
 // html에서 review 조회하는 함수
 let drawHtml = () => {
   // localStorage에 저장된 모든 값들을 불러오기
@@ -110,31 +87,11 @@ let drawHtml = () => {
     localStorageData[key] = JSON.parse(value); // JSON으로 변환 후 배열에 저장
   });
 
-  // 객체를 배열로 변환해서 정렬하고 다시 배열을 객체로 변환하면!!!!
-  // 객체에서는 정렬이.. 풀려버려....
-  // 배열을 Map으로 바꿔서 써야만.. 하는 것일까...??
-  // // 객체를 배열로 변환하여 comment_currentDate를 기준으로 정렬
-  // const sortedComments = Object.entries(localStorageData).sort((a, b) => {
-  //   const dateA = new Date(a[1].comment_currentDate);
-  //   const dateB = new Date(b[1].comment_currentDate);
-  //   return dateA - dateB;
-  // });
-
-  // console.log(sortedComments);
-
-  // // 정렬된 배열을 다시 객체로 변환
-  // const sortedlocalStorageData = {};
-  // sortedComments.forEach(([key, value]) => {
-  //   sortedlocalStorageData[key] = value;
-  // });
-
-  // console.log(sortedlocalStorageData);
-
   // 객체를 Map으로 변환
-  const commentsMap = new Map(Object.entries(localStorageData));
+  commentsMap = new Map(Object.entries(localStorageData));
 
   // Map을 comment_currentDate를 기준으로 정렬
-  const sortedCommentsMap = new Map([...commentsMap.entries()].sort((a, b) => {
+  sortedCommentsMap = new Map([...commentsMap.entries()].sort((a, b) => {
     const dateA = new Date(a[1].comment_currentDate);
     const dateB = new Date(b[1].comment_currentDate);
     return dateB - dateA;
@@ -155,7 +112,7 @@ let drawHtml = () => {
 
   let countStarScore = 0;
   let count = 0;
-  const valuesIterator = sortedCommentsMap.values(); // map의 value 값으로 배열 생성
+  valuesIterator = sortedCommentsMap.values(); // map의 value 값으로 배열 생성
 
   // 모든 댓글 추가
   for (const value of valuesIterator) {
@@ -172,21 +129,19 @@ let drawHtml = () => {
             <img src="./contents/review_btn.png" alt="Button Icon" />
           </button>
           <div class="review_edit_btn_wrapper" id="review_modal_edit${count}">
-            <button class="review_modal_edit_btn" id="review_edit_btn1${count}"> 수정하기 </button>
-            <button class="review_modal_delete_btn" id="review_delete_btn1${count}"> 삭제하기 </button>
-            <div class="review_modal_etc">
-              <span>아이디</span><input id="review_edit_id" /> <span>비밀번호</span
-              ><input type="password" id="review_edit_pw" />
-              <button id="submit_btn" type="button">등록</button>
-            </div>
+            <button class="review_modal_edit_btn" id="review_edit_btn1${count}" data-target="review_etc_modal${count}"> 수정하기 </button>
+            <button class="review_modal_delete_btn" id="review_delete_btn1${count}" data-target="review_etc_modal${count}"> 삭제하기 </button>
           </div>
+          <div class="review_modal_etc" id="review_etc_modal${count}" >
+              <span>비밀번호</span><input type="password" id="review_edit_pw${count}" class="review_etc_input" />
+              <button id="submit_btn${count}" class="submit_btn" type="button">확인</button>
+            </div>
         </div>
       </div>
       <p class="review_comment_box_content">${value.comment_comment}</p>
       <div class="review_comment_box_bottom">
-        <span class="review_comment_id"></span>
-        <span class="review_comment_id">${value.comment_id}</span>
-        <span class="review_comment_date">${value.comment_currentDate}</span>
+        <span class="review_comment_id" id="review_comment_id${count}">${value.comment_id}</span>
+        <span class="review_comment_date" id="review_comment_pw${count}">${value.comment_currentDate}</span>
       </div>
     </div>
   </li>
@@ -198,41 +153,109 @@ let drawHtml = () => {
   $id_avg_star_score.innerText = `${(countStarScore / countReview).toFixed(1)}점`;
 }
 
+// ... 버튼을 눌렀을 때
 document.addEventListener("DOMContentLoaded", function () {
   const $edit_btn = document.querySelectorAll('.review_edit_btn');
-
   $edit_btn.forEach((button) => {
     button.addEventListener("click", () => {
-      console.log("버튼이 클릭되었습니다.");
+      console.log("... 버튼이 클릭되었습니다.");
       const target = button.getAttribute("data-target");
       const modal = document.getElementById(target);
 
+      // edit
       if (modal.style.display === "none" || modal.style.display === "") {
         modal.style.display = "block"; // 표시
       } else {
         modal.style.display = "none"; // 숨김
       }
+
+      // 계정 입력 modal 숨김
+      const target_btn = target.replace("review_modal_edit", "review_etc_modal");
+      const $review_edit_btn = document.getElementById(target_btn);
+      $review_edit_btn.style.display = "none";
     });
   });
 });
 
+let edit_stats;
+let target;
+let target_num;
+// 수정하기 버튼을 눌렀을 때
 document.addEventListener("DOMContentLoaded", function () {
   const $modal_edit_btn = document.querySelectorAll('.review_modal_edit_btn');
 
   $modal_edit_btn.forEach((button) => {
     button.addEventListener("click", () => {
-      console.log("버튼이 클릭되었습니다.");
-      const target = button.getAttribute("data-target");
+      console.log("수정하기 버튼이 클릭되었습니다.");
+      target = button.getAttribute("data-target");
+      target_num = target.slice(-1);
+      // 편집 modal의 id 조회하기
       const modal = document.getElementById(target);
+      modal.style.display = "block";
+      // 
+      const target_btn = target.replace("review_etc_modal", "review_modal_edit");
+      const $review_edit_btn = document.getElementById(target_btn);
+      $review_edit_btn.style.display = "none";
 
-      if (modal) {
-        if (modal.style.display === "none" || modal.style.display === "") {
-          modal.style.display = "block"; // 표시
-        } else {
-          modal.style.display = "none"; // 숨김
-        }
-      } else {
-        console.log(`ID가 '${target}'인 요소를 찾을 수 없습니다.`);
+      edit_stats = "edit";
+    });
+  });
+});
+
+// 삭제하기 버튼을 눌렀을 때
+document.addEventListener("DOMContentLoaded", function () {
+  const $modal_edit_btn = document.querySelectorAll('.review_modal_delete_btn');
+
+  $modal_edit_btn.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log("삭제하기 버튼이 클릭되었습니다.");
+      target = button.getAttribute("data-target");
+      target_num = target.slice(-1);
+      // 의 id 조회한 후 숨기기
+      const modal = document.getElementById(target);
+      modal.style.display = "block";
+      // 편집 modal의 id 조회한 후 숨기기
+      const target_btn = target.replace("review_etc_modal", "review_modal_edit");
+      const $review_edit_btn = document.getElementById(target_btn);
+      $review_edit_btn.style.display = "none";
+
+      edit_stats = "delete";
+    });
+  });
+});
+
+// 댓글 편집 시 확인 버튼을 눌렀을 때
+document.addEventListener("DOMContentLoaded", function () {
+  const $buttons = document.querySelectorAll('.submit_btn');
+
+  $buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // 선택한 댓글의 key값 가져오기
+      const selectKey = sortedCommentsMap[0];
+      console.log(selectKey);
+
+      // 선택한 댓글의 비밀번호 가져오기
+      const selectValue = [...commentsMap.values()][target_num].comment_pw;
+      console.log(selectValue);
+
+      // 작성한 비밀번호 가져오기
+      const target_pw = target.replace("review_etc_modal", "review_edit_pw");
+      const $review_edit_pw = document.getElementById(target_pw);
+      const input_edit_pw = $review_edit_pw.value
+      console.log(input_edit_pw);
+
+      if (selectValue != input_edit_pw) {
+        console.log("비밀번호가 다르잖습니까!!");
+      } else if (edit_stats === "edit") {
+        // 수정일 때
+        console.log("수정 버튼이 클릭되었습니다.");
+        // review_modal_etc를 감춤
+        // 별점과 댓글을 수정하는 창을 감춤 해제
+
+      } else if (edit_stats === "delete") {
+        // 삭제일 때
+        console.log("삭제 버튼이 클릭되었습니다.");
+        // localStorage.removeItem('key');
       }
     });
   });
