911e6260dd03e898bad9a7d0efef17d4459ac8a5
Junsik <chzhgod@gmail.com>
2023-10-27 18:00:44 +0900

    메인 완료

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { searching } from "./search.js";
+import { searching, changeCnt } from "./search.js";
 import { generateMovieCards, movies, moreView } from "./card.js";
 generateMovieCards(movies);
 moreView();
+
+
 
 const $searchBox = document.querySelector(".search_box");
 // 검색 기능
@@ -9,3 +11,4 @@ $searchBox.addEventListener("submit", function (event) {
   event.preventDefault();
   searching();
 });
+
