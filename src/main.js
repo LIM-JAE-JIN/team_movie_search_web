36320598dcc4c3a51fd8074ad09e09aae62f2912
Junsik <chzhgod@gmail.com>
2023-10-27 14:24:13 +0900

    제발 되라

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { generateMovieCards, movies, moreView } from "./card.js";
 generateMovieCards(movies);
 moreView();
+
+const $searchBox = document.querySelector(".search_box");
+// 검색 기능
+$searchBox.addEventListener("submit", function (event) {
+  event.preventDefault();
+  searching();
+});
